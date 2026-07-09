5b2de9a11d3d6c3b2a556abcdd8625ec5bf17b95
chirag <chiragjangra999@gmail.com>
2026-07-09 22:18:22 +0530

    Fix CORS for Vercel frontend

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://localhost:5173', 'http://127.0.0.1:3000'],
+  origin: ['http://localhost:3000', 'http://localhost:5173', 'http://127.0.0.1:3000', 'https://nirogitanman-healthcare-center.vercel.app'],
   credentials: true
 }));
 app.use(express.json({ limit: '50mb' }));
